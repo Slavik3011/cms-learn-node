8d34c18adf4183247a65dca138ab64b47ee836ae
Slavik3011 <Slavik3011@yandex.ru>
2018-04-03 02:50:57 +0300

    add admin

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
+const port = process.env.port || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Set view engine
 app.engine('handlebars', hbs({ defaultLayout: 'home' }));
 app.set('view engine', 'handlebars');
 
-const main = require('./routes/home/main');
+// Load routes
+const home = require('./routes/home');
+const admin = require('./routes/admin');
 
-app.use('/', main);
+// Use routes
+app.use('/', home);
+app.use('/admin', admin);
 
-app.listen(3000, () => {
-  console.log(`Server started on port ${3000}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
